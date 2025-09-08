d0224795907093d645e4dfea4860aaa22574a0c3
wesley-cheung-ics4ui-01-25 <166552362+wesley-cheung-ics4ui-01-25@users.noreply.github.com>
2025-09-08 18:48:21 +0000

    submitting practiceproblems

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,5 +1,7 @@
-public class PracticeProblem {
+import java.util.Scanner; 
 
+public class PracticeProblem {
+	static Scanner input = new Scanner(System.in);
 	public static void main(String args[]) {
 		q1();
 		q2();
@@ -7,35 +9,53 @@ public class PracticeProblem {
 		q4();
 		q5();
 		q6();
+		input.close();
 	}
 
 	public static void q1() {
-		//Write question 1 code here
+		System.out.print("In: ");
+		String response = input.nextLine();
+		System.out.println(response);
 		
 	}
 
 	public static void q2() {
-		//Write question 2 code here
+		System.out.print("In: ");
+		int number1 = input.nextInt();
+		input.nextLine();
+		int number2 = input.nextInt();
+		input.nextLine();
+		System.out.println(number1/number2);
+		System.out.println(number1%number2);
 		
 	}
 
 	public static void q3() {
-		//Write question 3 code here
-		
+		System.out.print("In: ");
+		String response = input.nextLine();
+		System.out.println(response + " was the text you wrote");
 	}
 
 	public static void q4() {
-		//Write question 4 code here
-		
+		System.out.print("In: ");
+		int number1 = input.nextInt();
+		input.nextLine();
+		System.out.println(number1*5);
 	}
 
 	public static void q5() {
-		//Write question 5 code here
+		System.out.print("In: ");
+		boolean response = input.nextBoolean();
+		input.nextLine();
+		System.out.println(response + " is a boolean");
 		
 	}
 
 	public static void q6() {
-		//Write question 6 code here
+		System.out.print("In: ");
+		double number1 = input.nextDouble();
+		input.nextLine();
+		System.out.println(number1-3.2);
 		
 	}
 
